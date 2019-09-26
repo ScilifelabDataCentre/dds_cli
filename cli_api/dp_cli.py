fa0f5b544f3c6dd8932c9bcb21ee856715d18735
inaod568 <ina.oden.osterbo@gmail.com>
2019-09-26 17:04:17 +0200

    name changes and file structure in dp_cli.py. currently not working.

--- cli_api/dp_cli.py
+++
@@ -1,8 +0,0 @@
-import click
-
-@click.command()
-@click.option('--file', '-f', multiple=True, help='File to upload. Eg: "-file /path/to/file/filetoupload.xxx". Don\'t forget the If multiple files, use option multiple times.')
-def cli(file):
-    """Example script."""
-    click.echo('Hello World!')
-    click.echo(file)

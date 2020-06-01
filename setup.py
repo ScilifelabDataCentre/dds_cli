a26a12517c77afa82df18799a19f38b77ee25bb2
inaod568 <ina.oden.osterbo@gmail.com>
2020-06-01 16:16:00 +0200

    setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='code',
+    name='code_api',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
@@ -12,9 +12,12 @@ setup(
         'cryptography', 
         'requests',
     ],
+    dependency_links=[
+        'git+https://github.com/EGA-archive/crypt4gh.git', 
+    ],
     entry_points={
         'console_scripts': [
-            'dp_cli = code.dp_cli:cli',
+            'dp_cli = code_api.dp_cli:cli',
         ],
     },
 )

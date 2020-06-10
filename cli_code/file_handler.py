163e15c385956ce566fe72adb0c41ec7b6ecbef6
inaod568 <ina.oden.osterbo@gmail.com>
2020-06-10 17:49:41 +0200

    timing

--- cli_code/file_handler.py
+++ cli_code/file_handler.py
@@ -1,4 +1,5 @@
 import gzip
+import zstandard as zstd
 import sys
 import os
 from pathlib import Path
@@ -9,9 +10,17 @@ from nacl.bindings import (crypto_aead_chacha20poly1305_ietf_encrypt,
 from nacl.exceptions import CryptoError
 
 
-def compress_file(file: Path, chunk_size: int = 65536):
+def compress_file_gzip(file: Path, chunk_size: int = 65536):
+    print("chunk size --- ", chunk_size)
     for chunk in iter(lambda: file.read(chunk_size), b''):
         yield gzip.compress(data=chunk, compresslevel=9)
+
+
+def compress_file(file: Path, chunk_size: int = 65536):
+    cctzx = zstd.ZstdCompressor(write_checksum=True, level=4)
+    with cctzx.stream_reader(file) as reader:
+        for chunk in iter(lambda: reader.read(chunk_size), b''):
+            yield chunk
 
 
 def file_reader(file: Path, chunk_size: int = 65536):
@@ -32,31 +41,34 @@ def aead_encrypt_chacha(gen, key):
                                                                key=key)
 
 
-def prep_upload(file: Path, filedir: Path):
+def prep_upload(file: Path, filedir: Path = Path(""), chunk_size: int = 65536):
     '''Prepares the files for upload'''
 
     proc_suff = ""  # Suffix after file processed
     key = os.urandom(32)
 
     # Original file size
-    if not isinstance(file, Path):
-        pass  # update dict with error
+    # if not isinstance(file, Path):
+    #     pass  # update dict with error
 
-    if not file.exists():
-        pass  # update dict with error
+    # if not file.exists():
+    #     pass  # update dict with error
 
-    o_size = file.stat().st_size  # Bytes
+    # o_size = file.stat().st_size  # Bytes
 
     # Check if compressed
     compressed = False
-    if compressed:
-        proc_suff += ".gzip"
-    proc_suff += ".ccp1"
-    outfile = filedir / Path(file.name + proc_suff)
+    print(chunk_size)
+    # if compressed:
+    #     proc_suff += ".gzip"
+    # proc_suff += ".ccp1"
+    # outfile = filedir / Path(file.name + proc_suff)
+    outfile = Path("test_encrypted.xxx")
 
     # Read file
     with file.open(mode='rb') as f:
-        chunk_stream = file_reader(f) if compressed else compress_file(f)
+        chunk_stream = file_reader(f) if compressed else compress_file(
+            f, chunk_size=chunk_size)
         with outfile.open(mode='ab+') as outfile:
             for nonce, ciphertext in aead_encrypt_chacha(chunk_stream, key):
                 outfile.write(nonce)

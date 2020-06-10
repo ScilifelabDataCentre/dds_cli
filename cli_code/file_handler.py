e07f9cfe10441990c0aca3443f6edb14e07fcb1c
inaod568 <ina.oden.osterbo@gmail.com>
2020-06-10 19:22:50 +0200

    zstd and chacha, removed hashing for now

--- cli_code/file_handler.py
+++ cli_code/file_handler.py
@@ -10,12 +10,6 @@ from nacl.bindings import (crypto_aead_chacha20poly1305_ietf_encrypt,
 from nacl.exceptions import CryptoError
 
 
-def compress_file_gzip(file: Path, chunk_size: int = 65536):
-    print("chunk size --- ", chunk_size)
-    for chunk in iter(lambda: file.read(chunk_size), b''):
-        yield gzip.compress(data=chunk, compresslevel=9)
-
-
 def compress_file(file: Path, chunk_size: int = 65536):
     cctzx = zstd.ZstdCompressor(write_checksum=True, level=4)
     with cctzx.stream_reader(file) as reader:
@@ -48,52 +42,39 @@ def prep_upload(file: Path, filedir: Path = Path(""), chunk_size: int = 65536):
     key = os.urandom(32)
 
     # Original file size
-    # if not isinstance(file, Path):
-    #     pass  # update dict with error
+    if not isinstance(file, Path):
+        pass  # update dict with error
 
-    # if not file.exists():
-    #     pass  # update dict with error
+    if not file.exists():
+        pass  # update dict with error
 
-    # o_size = file.stat().st_size  # Bytes
+    o_size = file.stat().st_size  # Original size in bytes
 
-    # Check if compressed
+    # Check if compressed and save algorithm info if yes
     compressed = False
-    print(chunk_size)
-    # if compressed:
-    #     proc_suff += ".gzip"
-    # proc_suff += ".ccp1"
-    # outfile = filedir / Path(file.name + proc_suff)
-    outfile = Path("test_encrypted.xxx")
+    if compressed:
+        proc_suff += ".zstd"
+    proc_suff += ".ccp"
+    outfile = filedir / Path(file.name + proc_suff)
 
     # Read file
-    with file.open(mode='rb') as f:
-        chunk_stream = file_reader(f) if compressed else compress_file(
-            f, chunk_size=chunk_size)
-        with outfile.open(mode='ab+') as outfile:
-            for nonce, ciphertext in aead_encrypt_chacha(chunk_stream, key):
-                outfile.write(nonce)
-                outfile.write(ciphertext)
-
-    # Compress
-
-    # Check compressed file size
-
-    # Encrypt
+    try:
+        original_umask = os.umask(0)
+        with file.open(mode='rb') as f:
+            # Should we hash the file and save to file before comp and enc?
+            # Compress if not compressed
+            chunk_stream = file_reader(f) if compressed else compress_file(f)
+            # Encrypt
+            with outfile.open(mode='ab+') as outfile:
+                for nonce, ciphertext in aead_encrypt_chacha(chunk_stream, key):
+                    outfile.write(nonce)
+                    outfile.write(ciphertext)
+    except Exception as ee:  # FIX EXCEPTION
+        return file, "Error", ee
+    finally:
+        os.umask(original_umask)
 
     # Check encrypted file size
+    e_size = outfile.stat().st_size  # Encrypted size in bytes
 
-    # hash
-    # _, checksum = gen_hmac(file=file)
-
-    # encrypt
-    # encrypted_file = self.tempdir.files / Path(file.name + ".c4gh")
-    # try:
-    #     original_umask = os.umask(0)
-    #     with file.open(mode='rb') as orig:
-
-    # except Exception as ee:
-    #     return file, "Error", ee
-    # finally:
-    #     os.umask(original_umask)
-
-    # return file, encrypted_file, checksum
+    return file, outfile, ""

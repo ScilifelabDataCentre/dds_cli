6f1f246ce77445ed74afae4f2338332ac37e039f
inaod568 <ina.oden.osterbo@gmail.com>
2021-02-05 11:30:42 +0100

    ProjectAccess fixed

--- cli_code/data_deliverer.py
+++ cli_code/data_deliverer.py
@@ -14,6 +14,7 @@ import json
 # Installed
 
 # Own modules
+from cli_code import user
 
 ###############################################################################
 # START LOGGING CONFIG ################################# START LOGGING CONFIG #
@@ -26,21 +27,25 @@ LOG.setLevel(logging.DEBUG)
 # CLASSES ########################################################### CLASSES #
 ###############################################################################
 
+
 class DataDeliverer:
     """Data deliverer class."""
 
-    def __init__(self, *args, **kwargs): 
-        
+    def __init__(self, *args, **kwargs):
+
         # Quit if no delivery info is specified
         if not kwargs:
             sys.exit("Missing Data Delivery System user credentials.")
-        
+
         self.method = sys._getframe().f_back.f_code.co_name
         LOG.debug("Method: %s", self.method)
 
         # Get user info
         username, password, project, recipient = \
             self.verify_input(user_input=kwargs)
+
+        dds_user = user.User(username=username, password=password,
+                             project=project, recipient=recipient)
 
     def verify_input(self, user_input):
         """Verifies that the users input is valid and fully specified."""
@@ -50,24 +55,28 @@ class DataDeliverer:
         project = None
         recipient = None
 
+        # Get user info from kwargs
         if "username" in user_input and user_input["username"]:
             username = user_input["username"]
         if "project" in user_input and user_input["project"]:
             project = user_input["project"]
         if "recipient" in user_input and user_input["recipient"]:
             recipient = user_input["recipient"]
-        
+
+        # Get contents from file
         if "config" in user_input and user_input["config"]:
             configpath = pathlib.Path(user_input["config"]).resolve()
             if not configpath.exists():
                 sys.exit("Config file does not exist.")
-            
+
+            # Get contents from file
             try:
                 with configpath.open(mode="r") as cfp:
-                    from django.conf import settings = json.load(cfp)
+                    contents = json.load(cfp)
             except json.decoder.JSONDecodeError as err:
                 sys.exit(f"Failed to get config file contents: {err}")
 
+            # Get user credentials and project info if not already specified
             if username is None and "username" in contents:
                 username = contents["username"]
             if project is None and "project" in contents:
@@ -77,14 +86,16 @@ class DataDeliverer:
 
             if "password" in contents:
                 password = contents["password"]
-            
+
+        # Username and project info is minimum required info
         if None in [username, project]:
             sys.exit("Data Delivery System options are missing.")
-        
+
         if password is None:
             password = getpass.getpass()
 
+        # Recipient required for upload
         if self.method == "put" and recipient is None:
             sys.exit("Project owner/data recipient not specified.")
-        
+
         return username, password, project, recipient
--- cli_code/user.py
+++ cli_code/user.py
@@ -31,75 +31,24 @@ LOG = logging.getLogger(__name__)
 class User:
     """Authenticates the DDS user."""
 
-    def __init__(self, config=None, username=None, project_id=None,
-                 recipient=None):
-
-        # Quit if no user credentials appears to be specified
-        if config is None and username is None:
-            sys.exit("Missing Data Delivery System user credentials.")
-
-        # Define method - put
-        self.method = sys._getframe().f_back.f_code.co_name
-        LOG.debug("Method: %s", self.method)
-
-        # Get user info
-        username, password, project, recipient = self.verify_input(
-            config=config, username=username,
-            project=project_id, recipient=recipient
-        )
+    def __init__(self, username=None, password=None, project=None,
+                 recipient=None, method=None):
 
         # Authenticate user
-        token = self.authenticate_user(username=username, password=password)
+        token = self.authenticate_user(username=username, password=password,
+                                       facility=recipient is not None)
+        print(token)
 
         # Approve project
-        # approved = self.verify_project_access(project=project,
-        #                                       username=username,
-        #                                       token=token)
+        approved = self.verify_project_access(project=project,
+                                              token=token)
 
-    def verify_input(self, config, username, project, recipient):
-        """Verifies that the users input is valid and fully specified."""
-
-        password = None
-
-        # Get info from config file if specified
-        if config:
-            configpath = pathlib.Path(config).resolve()
-            if not configpath.exists():
-                sys.exit("Config file does not exist.")
-
-            # Get contents from file
-            try:
-                with configpath.open(mode="r") as cfp:
-                    contents = json.load(cfp)
-            except json.decoder.JSONDecodeError as err:
-                sys.exit(f"Failed to get config file contents: {err}")
-
-            # Get user credentials and project info
-            if username is None and "username" in contents:
-                username = contents["username"]
-            if project is None and "project" in contents:
-                project = contents["project"]
-            if recipient is None and "recipient" in contents:
-                recipient = contents["recipient"]
-
-            if "password" in contents:
-                password = contents["password"]
-
-        if None in [username, project]:
-            sys.exit("Data Delivery System options missing.")
-
-        if password is None:
-            password = getpass.getpass()
-
-        if self.method == "put" and recipient is None:
-            sys.exit("Project owner/data recipient not specified.")
-
-        return username, password, project, recipient
-
-    def authenticate_user(self, username, password):
+    def authenticate_user(self, username, password, facility):
         """Authenticates the username and password via a call to the API."""
 
-        response = requests.get(DDSEndpoint.AUTH, auth=(username, password))
+        response = requests.get(DDSEndpoint.AUTH,
+                                params={"facility": facility},
+                                auth=(username, password))
 
         if not response.ok:
             sys.exit("User authentication failed! "
@@ -109,7 +58,7 @@ class User:
         token = response.json()
         return {"x-access-token": token["token"]}
 
-    def verify_project_access(self, project, username, token):
+    def verify_project_access(self, project, token):
         """docstring"""
 
         response = requests.get(DDSEndpoint.AUTH_PROJ,

2c216f6746f9fd87f85d00ccd30531012eb5e0f2
inaod568 <ina.oden.osterbo@gmail.com>
2021-02-12 11:08:08 +0100

    generate bucket filepath + compute subpath

--- cli_code/file_handler.py
+++ cli_code/file_handler.py
@@ -18,8 +18,8 @@ import uuid
 # START LOGGING CONFIG ################################# START LOGGING CONFIG #
 ###############################################################################
 
-LOG = logging.getLogger(__name__)
-LOG.setLevel(logging.DEBUG)
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 ###############################################################################
 # CLASSES ########################################################### CLASSES #
@@ -50,19 +50,57 @@ class FileCollector:
         if not data_list:
             sys.exit("No data specified.")
 
-        self.data = self.collect_file_info_local(data=data_list)
+        self.data = self.collect_file_info_local(all_paths=data_list)
+        for x, y in self.data.items():
+            log.debug("%s : %s\n", x, y)
+        sys.exit()
 
-    def generate_new_filename(self, file_name):
-        """Generates filename which the file will be called in the bucket."""
+    def collect_file_info_local(self, all_paths, folder=None):
+        """Get info on each file in each path specified."""
 
-        return "".join([str(uuid.uuid4().hex[:6]), file_name])
+        file_info = dict()
 
-    def collect_file_info_local(self, data):
-        """Gets information relating to all specified paths."""
+        for path in all_paths:
+            # Get info for all files
+            # and feed back to same function for all folders
+            if path.is_file():
+                subpath = self.compute_subpath(file=path, folder=folder)
+                file_info[path] = {
+                    "subpath": subpath,
+                    "name_in_bucket": self.generate_bucket_filepath(
+                        filename=path.name,
+                        folder=folder
+                    ),
+                    "name_in_db": subpath / path.name
+                }
+            elif path.is_dir():
+                file_info.update({
+                    **self.collect_file_info_local(all_paths=path.glob("**/*"),
+                                                   folder=path.name)
+                })
 
-        all_files = {}
-        for x in data:
-            if x.is_file():
-                all_files[x] = {}
+        return file_info
 
-        return data
+    def generate_bucket_filepath(self, filename="", folder=None):
+        """Generates filename and new path which the file will be
+        called in the bucket."""
+
+        # Set path to file
+        directory = pathlib.Path("") if folder is None \
+            else pathlib.Path(folder)
+
+        # Generate new file name
+        new_name = "".join([str(uuid.uuid4().hex[:6]), "_", filename])
+
+        return str(directory / pathlib.Path(new_name))
+
+    def compute_subpath(self, file, folder):
+        """Computes the path to the file, from the specified folder."""
+
+        subdir = pathlib.Path("")
+        if folder is not None:
+            fileparts = file.parts
+            start_ind = fileparts.index(folder)
+            subdir = pathlib.Path(*fileparts[start_ind:-1])
+
+        return subdir

0d3e4ef4c60910e810843d3bf57dc737f4a5edfb
inaod568 <ina.oden.osterbo@gmail.com>
2021-04-17 11:47:38 +0200

    processpoolexecutor performing new function

--- cli_code/dds_exceptions.py
+++
@@ -1,9 +0,0 @@
-"""Exceptions specific to the Data Delivery System.
-
-Used in places where there are no other suitable Exceptions, either general or module
-specific.
-"""
-
-
-class ChecksumError(Exception):
-    """Errors regarding the generation of checksums."""

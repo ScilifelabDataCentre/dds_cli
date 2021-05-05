cf84e95b683e1f853785c0ba0f22a66e0aa4d42e
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2021-05-05 14:46:42 +0200

    endpoint based env variable

--- cli_code/__init__.py
+++ cli_code/__init__.py
@@ -75,8 +75,11 @@ class DDSEndpoint:
 
     BASE_ENDPOINT_LOCAL = "http://127.0.0.1:5000/api/v1"
     BASE_ENDPOINT_REMOTE = "https://dds.dckube.scilifelab.se/api/v1"
-
-    BASE_ENDPOINT = BASE_ENDPOINT_REMOTE
+    
+    if os.getenv("DS_CLI_ENV") == "development":
+        BASE_ENDPOINT = BASE_ENDPOINT_LOCAL
+    else:
+        BASE_ENDPOINT = BASE_ENDPOINT_REMOTE
 
     AUTH = BASE_ENDPOINT + "/user/auth"
     AUTH_PROJ = BASE_ENDPOINT + "/proj/auth"

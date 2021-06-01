d89dd922122aa68a514cf86827b4cb8cd8d2bd73
Phil Ewels <phil.ewels@scilifelab.se>
2021-06-01 20:57:07 +0200

    Compress new ascii logo a little

--- dds_cli/__main__.py
+++ dds_cli/__main__.py
@@ -51,15 +51,15 @@ console = rich.console.Console()
 
 # Print header to STDERR
 stderr = rich.console.Console(stderr=True)
-stderr.print("\n[green]      ︵", highlight=False)
-stderr.print("[green] ︵  (  )    ︵", highlight=False)
-stderr.print("[green](  )  ) (   (  )[/]    [bold]SciLifeLab Data Delivery System", highlight=False)
+stderr.print("\n[green]     ︵", highlight=False)
+stderr.print("[green] ︵ (  )   ︵", highlight=False)
+stderr.print("[green](  ) ) (  (  )[/]   [bold]SciLifeLab Data Delivery System", highlight=False)
 stderr.print(
-    "[green] ︶   (  )  ) ([/]     [blue][link=https://www.scilifelab.se/data]https://www.scilifelab.se/data[/link]",
+    "[green] ︶  (  ) ) ([/]    [blue][link=https://www.scilifelab.se/data]https://www.scilifelab.se/data[/link]",
     highlight=False,
 )
-stderr.print(f"[green]       ︶  (  )[/]     [dim]Version {dds_cli.__version__}", highlight=False)
-stderr.print("[green]            ︶\n", highlight=False)
+stderr.print(f"[green]      ︶ (  )[/]    [dim]Version {dds_cli.__version__}", highlight=False)
+stderr.print("[green]          ︶\n", highlight=False)
 
 
 @click.group()

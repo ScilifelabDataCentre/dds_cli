12a4192571d81a385d6a74fbaa1beb64b25839c0
Phil Ewels <phil.ewels@scilifelab.se>
2021-06-02 23:03:36 +0200

    Black - exceptions.py

--- dds_cli/exceptions.py
+++ dds_cli/exceptions.py
@@ -6,9 +6,12 @@ class AuthenticationError(Exception):
 
     Return the message with Rich no-entry-sign emoji either side.
     """
+
     def __str__(self):
         return f"\n:no_entry_sign: {self.message} :no_entry_sign:\n"
 
+
 class UploadError(Exception):
     """Errores relating to file uploads"""
+
     pass

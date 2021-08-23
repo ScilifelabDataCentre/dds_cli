d06da85200ed3b6575e6516509a41ebf86253b2b
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-08-23 11:46:26 +0200

    testing

---
+++ tests/test_base.py
@@ -0,0 +1,71 @@
+# Standard library
+
+# Installed
+import pytest
+
+# Own modules
+from dds_cli import base
+from dds_cli import exceptions
+
+
+def test_ddsbaseclass_no_method(monkeypatch):
+    """No method should raise an error indicating an invalid method."""
+
+    with pytest.raises(exceptions.InvalidMethodError):
+        _ = base.DDSBaseClass()
+
+
+def test_ddsbaseclass_method_ls_and_missing_username(monkeypatch):
+    """Missing username should raise exception"""
+
+    with pytest.raises(exceptions.MissingCredentialsException) as mis_err:
+        _ = base.DDSBaseClass(method="ls")
+
+    assert "missing" in str(mis_err.value)
+    assert "username" in str(mis_err.value)
+
+
+def test_ddsbaseclass_method_ls_and_missing_password(monkeypatch):
+    """Missing password should raise exception indicating too little user info."""
+
+    monkeypatch.setattr("getpass.getpass", lambda: None)
+    with pytest.raises(exceptions.MissingCredentialsException) as mis_err:
+        _ = base.DDSBaseClass(method="ls", username="test")
+
+    assert "missing" in str(mis_err.value)
+
+
+def test_ddsbaseclass_method_ls_and_incorrect_username(monkeypatch):
+    """Incorrect username with password should result in AuthenticationError"""
+
+    monkeypatch.setattr("getpass.getpass", lambda: str("password"))
+    with pytest.raises(exceptions.AuthenticationError) as autherr:
+        _ = base.DDSBaseClass(method="ls", username="test")
+
+    assert "Incorrect" in str(autherr.value)
+
+
+def test_ddsbaseclass_method_ls_and_incorrect_password(monkeypatch):
+    """Incorrect password should result in AuthenticationError"""
+
+    monkeypatch.setattr("getpass.getpass", lambda: str("incorrect_password"))
+    with pytest.raises(exceptions.AuthenticationError) as autherr:
+        _ = base.DDSBaseClass(method="ls", username="username")
+
+    assert "Incorrect" in str(autherr.value)
+
+
+def test_ddsbaseclass_method_ls_and_correct_info(monkeypatch):
+    """Correct info should result in a DDSBaseClass Object"""
+
+    monkeypatch.setattr("getpass.getpass", lambda: str("password"))
+    baseclass_object = base.DDSBaseClass(method="ls", username="username")
+    assert isinstance(baseclass_object, base.DDSBaseClass)
+    assert baseclass_object.method == "ls"
+    assert baseclass_object.project is None
+    assert baseclass_object.token and baseclass_object.token != ""
+    assert not hasattr(baseclass_object, "username")
+    assert not hasattr(baseclass_object, "password")
+    assert not hasattr(baseclass_object, "keys")
+    assert not hasattr(baseclass_object, "status")
+    assert not hasattr(baseclass_object, "filehandler")
---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+# Standard library
+
+# Installed
+
+# Own modules
+from dds_cli import __main__
+
+# # Tests
+# def test_list_projects():

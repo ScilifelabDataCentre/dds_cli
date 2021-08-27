68d2d513645ae4058635a75590cfee39525d690c
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-08-27 11:41:57 +0200

    test project validation with incorrect project

--- tests/test_requests/test_auth_proj_request.py
+++ tests/test_requests/test_auth_proj_request.py
@@ -10,9 +10,15 @@ import dds_cli
 import tests.tools.tools_for_testing
 
 # VARIABLES ############################################################################ VARIABLES #
-valid_token = tests.tools.tools_for_testing.get_valid_token()
+
 correct_project = "public_project_id"
 incorrect_project = "incorrect_project_id"
+
+valid_token_no_project = tests.tools.tools_for_testing.get_valid_token()
+valid_token_invalid_project = tests.tools.tools_for_testing.get_valid_token(
+    project=incorrect_project
+)
+valid_token_valid_project = tests.tools.tools_for_testing.get_valid_token(project=correct_project)
 
 # TESTS #################################################################################### TESTS #
 
@@ -45,12 +51,34 @@ def test_auth_proj_token_wrong_format():
 def test_auth_proj_no_method():
     """No method specified in request - required"""
 
-    response = requests.get(dds_cli.DDSEndpoint.AUTH_PROJ, headers=valid_token)
+    response = requests.get(dds_cli.DDSEndpoint.AUTH_PROJ, headers=valid_token_no_project)
     assert response.status_code == 400
     response_json = response.json()
     assert response_json.get("message")
     assert "No method found in request" in response_json.get("message")
 
 
-# def test_auth_proj_no_project():
-#     """Project ID required to attempt access"""
+def test_auth_proj_no_project():
+    """Project ID required to attempt access"""
+
+    response = requests.get(
+        dds_cli.DDSEndpoint.AUTH_PROJ, headers=valid_token_no_project, params={"method": "ls"}
+    )
+    assert response.status_code == 500
+    response_json = response.json()
+    assert response_json.get("message")
+    assert "Attempting to validate users project access without project ID" in response_json.get(
+        "message"
+    )
+
+
+def test_auth_proj_incorrect_project():
+    """Invalid project attempted"""
+
+    response = requests.get(
+        dds_cli.DDSEndpoint.AUTH_PROJ, headers=valid_token_invalid_project, params={"method": "ls"}
+    )
+    assert response.status_code == 400
+    response_json = response.json()
+    assert response_json.get("message")
+    assert "The specified project does not exist" in response_json.get("message")

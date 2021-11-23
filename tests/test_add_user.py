31e7749c51fe001784ced011f14a69f1f6ccc3d2
Johannes Alneberg <alneberg@kth.se>
2021-11-23 14:57:28 +0100

    Added a test for add user where request fails

--- tests/test_add_user.py
+++ tests/test_add_user.py
@@ -1,8 +1,8 @@
 # Standard library
-from unittest.mock import patch, MagicMock
+import unittest.mock
 
 # Installed
-from click.testing import CliRunner
+import click.testing
 import requests
 from dds_cli import DDSEndpoint
 
@@ -11,24 +11,46 @@ from dds_cli.__main__ import dds_main
 import dds_cli
 from dds_cli.user import User
 
+ADD_JSON = {"email": "test.testsson@example.com", "role": "Researcher"}
 
-@patch.object(requests, "post")
-@patch.object(User, "_User__retrieve_token")
-def test_add_user_no_project(mock_A, mock_B):
+
+@unittest.mock.patch.object(requests, "post")
+@unittest.mock.patch.object(User, "_User__retrieve_token")
+def test_add_user_no_project_OK(mock_A, mock_B):
     mock_A.return_value = None
-    mock_returned_request = MagicMock(status_code=200)
+    mock_returned_request = unittest.mock.MagicMock(status_code=200)
     mock_returned_request.json.return_value = {}
     mock_B.return_value = mock_returned_request
 
-    runner = CliRunner(mix_stderr=False)
-    add_user_email = "test.testsson@example.com"
-    add_user_role = "Researcher"
+    runner = click.testing.CliRunner(mix_stderr=False)
+
     result = runner.invoke(
         dds_main,
-        ["add-user", "-u", "unituser", "-e", add_user_email, "-r", add_user_role],
+        ["add-user", "-u", "unituser", "-e", ADD_JSON["email"], "-r", ADD_JSON["role"]],
         catch_exceptions=False,
     )
 
-    mock_B.assert_called()
-    print(result.stderr)
+    mock_B.assert_called_with(DDSEndpoint.USER_ADD, json=ADD_JSON, headers=unittest.mock.ANY)
+
     assert result.exit_code == 0
+
+
+@unittest.mock.patch.object(requests, "post")
+@unittest.mock.patch.object(User, "_User__retrieve_token")
+def test_add_user_no_project_fail(mock_A, mock_B):
+    mock_A.return_value = None
+    mock_returned_request = unittest.mock.MagicMock(status_code=403, ok=False)
+    mock_returned_request.json.return_value = {}
+    mock_B.return_value = mock_returned_request
+    runner = click.testing.CliRunner(mix_stderr=False)
+
+    result = runner.invoke(
+        dds_main,
+        ["add-user", "-u", "unituser", "-e", ADD_JSON["email"], "-r", ADD_JSON["role"]],
+        catch_exceptions=False,
+    )
+
+    mock_B.assert_called_with(DDSEndpoint.USER_ADD, json=ADD_JSON, headers=unittest.mock.ANY)
+
+    assert "Could not add user" in result.stderr
+    assert result.exit_code != 0
--- tests/test_something_empty.py
+++
@@ -1,5 +0,0 @@
-"""Pytest tests for DDS CLI."""
-
-
-def test_something():
-    """Test required so that the pytest don't fail."""

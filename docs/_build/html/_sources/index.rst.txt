2a88f5d416299a19ab9436232e7d6cf92ab13b45
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-12-06 13:07:11 +0100

    all docs

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,30 @@
+.. Data Delivery System documentation master file, created by
+   sphinx-quickstart on Mon Dec  6 10:33:02 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to the Data Delivery System's documentation!
+====================================================
+
+Check out the :doc:`usage` section for further information.
+
+Usage
+--------
+
+.. toctree::
+
+   usage
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: DDS CLI Modules :
+
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- docs/conf.py
+++ docs/conf.py
@@ -12,19 +12,15 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../dds_cli"))
 
 
 # -- Project information -----------------------------------------------------
-# pylint: disable=invalid-name, redefined-builtin
-project = "Data Delivery System"
-copyright = "2020, Data Centre"
-author = "Data Centre"
 
-# The full version, including alpha/beta/rc tags
-release = "0.0.1"
+project = "Data Delivery System"
+copyright = "2021, SciLifeLab Data Centre"
+author = "SciLifeLab Data Centre"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,7 +28,7 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon", "sphinx_click"]
+extensions = ["sphinx.ext.autodoc", "sphinx_click", "sphinx.ext.duration", "sphinx.ext.autosummary"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -40,7 +36,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,12 +44,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "classic"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-add_module_names = False
+html_css_files = ["custom.css"]

388394e4990cba1968ac820b79ae0852312ab58e
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2022-01-19 13:05:30 +0100

    message is already string

--- dds_cli/project_creator.py
+++ dds_cli/project_creator.py
@@ -60,7 +60,7 @@ class ProjectCreator(base.DDSBaseClass):
 
         # Error if failed
         if not response.ok:
-            raise exceptions.ProjectCreationError(f"{response.json().get('message')}")
+            raise exceptions.ProjectCreationError(response.json().get("message"))
 
         # Get response info as json
         try:

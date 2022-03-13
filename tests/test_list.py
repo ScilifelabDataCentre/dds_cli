9d7cbb536ff4891b24523d093fbb0fb41bfb71aa
Ina Odén Österbo <inaod568@users.noreply.github.com>
2022-03-13 16:47:43 +0100

    removed rich emoji

--- tests/test_list.py
+++ tests/test_list.py
@@ -144,8 +144,6 @@ def list_no_project_specified(ls_runner, list_request, command):
         "In Progress",
         "Tue, 23 Nov",
         "Wed, 24 Nov",
-        ":white_heavy_check_mark:",
-        ":white_heavy_check_mark:",
         "────────────────",  # Hack to test that there's a table printed
     ]:
         assert substring in result.stdout

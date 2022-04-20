c18ca856c1cf1c807c78a903a2a63b2cbac5dd5f
valyo <582646+valyo@users.noreply.github.com>
2022-04-20 08:47:55 +0200

    tests for ok and failed motd command

--- tests/test_motd.py
+++ tests/test_motd.py
@@ -16,7 +16,7 @@ def runner_motd(runner):
     """Run dds motd add."""
 
     def _run():
-        return runner(["motd", "add", "-m", ADD_JSON["message"]])
+         return runner(["motd", "add", ADD_JSON["message"]])
 
     yield _run
 
@@ -31,7 +31,6 @@ def add_motd():
     with unittest.mock.patch.object(requests, "post") as mock_motd:
 
         def _request_mock(status_code, message=None, ok=True):
-            #ToDo
             mock_returned_request = unittest.mock.MagicMock(status_code=status_code, ok=ok)
             mock_returned_request.json.return_value = {"message": message}
             mock_motd.return_value = mock_returned_request
@@ -41,14 +40,28 @@ def add_motd():
 
 
 def test_add_motd_OK(runner_motd, add_motd):
-    add_user_OK = add_motd(200)
+    add_motd_OK = add_motd(200)
     result = runner_motd()
-    add_user_OK.assert_called_with(
-        dds_cli.DDSEndpoint.USER_ADD,
-        json={**ADD_JSON, "send_email": True},
-        params={"project": None},
+    
+    assert result.exit_code == 0    
+    add_motd_OK.assert_called_with(
+        dds_cli.DDSEndpoint.ADD_NEW_MOTD,
+        json={**ADD_JSON},
         headers=unittest.mock.ANY,
         timeout=dds_cli.DDSEndpoint.TIMEOUT,
     )
 
-    assert result.exit_code == 0
+
+def test_add_motd_fail(runner_motd, add_motd):
+    add_motd_FAIL = add_motd(403, message="Passed message", ok=False)
+    result = runner_motd()
+    
+    assert result.exit_code != 0
+    assert "Only Super Admin can add a MOTD" in result.stderr
+    assert "Passed message" in result.stderr
+    add_motd_FAIL.assert_called_with(
+        dds_cli.DDSEndpoint.ADD_NEW_MOTD,
+        json={**ADD_JSON},
+        headers=unittest.mock.ANY,
+        timeout=dds_cli.DDSEndpoint.TIMEOUT,
+    )

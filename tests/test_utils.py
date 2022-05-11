97d731891d2427388596d1dbf8ac51a731997636
Ina <inaod568@users.noreply.github.com>
2022-05-11 13:38:54 +0200

    added test for errors

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,8 +10,8 @@ def test_perform_request_post_request() -> None:
     url: str = "http://localhost"
     with Mocker() as mock:
         mock.post(url, status_code=200, json={})
-        response: Response = perform_request(endpoint=url, headers={}, method="post")
-        assert response == {}
+        response: tuple(Response, str) = perform_request(endpoint=url, headers={}, method="post")
+        assert response[0] == {}
 
 
 def test_perform_request_project_creation_error() -> None:
@@ -83,3 +83,16 @@ def test_perform_request_add_motd_error_insufficient_credentials() -> None:
 
         assert len(exc_info.value.args) == 1
         assert exc_info.value.args[0] == "API Request failed.: Only Super Admin can add a MOTD."
+
+def test_perform_request_project_access_errors() -> None:
+    """Test that the `errors` in the response are parsed in the correct way."""
+    response_json: Dict = {
+        "email": "test@mail.com",
+        "errors": {"project_1": "test message", "project_2": "test message"},
+        "status": 400
+    }
+    with Mocker() as mock:
+        mock.post(DDSEndpoint.PROJ_ACCESS, status_code=400, json=response_json)
+        with raises(DDSCLIException) as exc_info:
+            perform_request(endpoint=DDSEndpoint.PROJ_ACCESS, headers={}, method="post", error_message="Project access error")
+        assert "Project access error\ntest message\n   - project_1\n   - project_2" in str(exc_info.value)

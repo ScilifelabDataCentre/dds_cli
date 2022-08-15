74279278ee37d718b8321613a235b0db246ba944
valyo <582646+valyo@users.noreply.github.com>
2022-08-15 17:08:44 +0200

    review suggestion
    Co-authored-by: Ina Odén Österbo <35953392+inaod568@users.noreply.github.com>

--- dds_cli/__main__.py
+++ dds_cli/__main__.py
@@ -91,7 +91,7 @@ dds_cli.utils.stderr_console.print(
     highlight=False,
 )
 
-if "motd" not in sys.argv:
+if len(sys.argv) > 1 and sys.argv[1] != "motd":
     motds = dds_cli.motd_manager.MotdManager.list_all_active_motds(table=False)
     if motds:
         dds_cli.utils.stderr_console.print(f"[bold]Important information:[/bold]")

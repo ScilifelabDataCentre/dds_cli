fa9bb77fc43002c34254a30e25905205b3212a96
valyo <582646+valyo@users.noreply.github.com>
2022-09-05 13:13:37 +0200

    change that makes format_api_response work

--- dds_cli/utils.py
+++ dds_cli/utils.py
@@ -119,7 +119,8 @@ def create_table(
             *[
                 rich.markup.escape(
                     dds_cli.utils.format_api_response(
-                        str(row[x]) if ints_as_string and isinstance(row[x], int) else row[x], x
+                        row[x],
+                        x,
                     )
                 )
                 for x in columns

005cb4be5db5b30a01d3e80d9a0846daa1497de3
Ina <inaod568@users.noreply.github.com>
2022-10-05 15:33:12 +0200

    tests for csv

--- tests/test_file_encryptor.py
+++ tests/test_file_encryptor.py
@@ -6,7 +6,7 @@ from dds_cli import FileSegment
 from dds_cli import file_handler_local
 from pyfakefs.fake_filesystem import FakeFilesystem
 import os
-
+import csv
 
 # Text files
 
@@ -152,3 +152,93 @@ def test_verify_checksum_images():
         not fake_1_altered_verified
         and message1altered == "Checksum verification failed. File compromised."
     )
+
+# Csv files
+
+def verify_files_csv(fs: FakeFilesystem, magnitude: str):
+    """Perform checksum generation and verification for files of specific sizes."""
+    # Specify number of lines to add
+    num_lines: int = 0
+    if magnitude == "less":
+        num_lines = 100
+    elif magnitude == "more":
+        num_lines = 10000
+    
+    # Define lines to input
+    cell_contents: str = "abcdefghijklmnopqrstuvwxyzåäö0123456789"
+    row_contents: typing.List = [cell_contents] * 10
+    file_contents: typing.List = [row_contents] * num_lines
+
+    # Create fake files and verify that they have same size
+    fake_file_path_1: pathlib.Path = pathlib.Path("small_text_file_1.csv")
+    fake_file_path_2: pathlib.Path = pathlib.Path("small_text_file_2.csv")
+    for file in [fake_file_path_1, fake_file_path_2]:
+        fs.create_file(file)
+        assert os.path.exists(file)
+        with file.open(mode="w") as f:
+            writer = csv.writer(f)
+            writer.writerows(file_contents)
+        if magnitude == "less":
+            assert os.stat(file).st_size < FileSegment.SEGMENT_SIZE_RAW
+        elif magnitude == "more":
+            assert os.stat(file).st_size > FileSegment.SEGMENT_SIZE_RAW
+    assert os.stat(fake_file_path_1).st_size == os.stat(fake_file_path_2).st_size
+
+    # Generate checksums
+    checksum_file_1 = hashlib.sha256()
+    for chunk in file_handler_local.LocalFileHandler.read_file(file=fake_file_path_1):
+        checksum_file_1.update(chunk)
+
+    checksum_file_2 = hashlib.sha256()
+    for chunk in file_handler_local.LocalFileHandler.read_file(file=fake_file_path_2):
+        checksum_file_2.update(chunk)
+
+    # Make sure they are identical
+    checksum_hexdigest_1 = checksum_file_1.hexdigest()
+    checksum_hexdigest_2 = checksum_file_2.hexdigest()
+    assert checksum_hexdigest_1 == checksum_hexdigest_2
+
+    # Make sure verify_checksum gives the same
+    fake_1_verified, message1 = file_encryptor.Encryptor.verify_checksum(
+        file=fake_file_path_1, correct_checksum=checksum_hexdigest_1
+    )
+    assert fake_1_verified and message1 == "File integrity verified."
+    fake_2_verified, message2 = file_encryptor.Encryptor.verify_checksum(
+        file=fake_file_path_2, correct_checksum=checksum_hexdigest_2
+    )
+    assert fake_2_verified and message2 == "File integrity verified."
+
+    # Change one file slightly
+    with fake_file_path_1.open(mode="a") as f:
+        writer = csv.writer(f)
+        writer.writerow(row_contents)
+    assert os.stat(fake_file_path_1).st_size > os.stat(fake_file_path_2).st_size
+
+    # Create new checksum for altered file
+    checksum_file_1_altered = hashlib.sha256()
+    for chunk in file_handler_local.LocalFileHandler.read_file(file=fake_file_path_1):
+        checksum_file_1_altered.update(chunk)
+
+    # Verify that checksum has changed
+    checksum_hexdigest_1_altered = checksum_file_1_altered.hexdigest()
+    assert checksum_hexdigest_1_altered != checksum_hexdigest_1
+
+    # Verify that verify_checksum fails to verify integrity
+    fake_1_altered_verified, message1altered = file_encryptor.Encryptor.verify_checksum(
+        file=fake_file_path_1, correct_checksum=checksum_hexdigest_1
+    )
+    assert (
+        not fake_1_altered_verified
+        and message1altered == "Checksum verification failed. File compromised."
+    )
+
+
+def test_verify_checksum_less_than_chunk_csv(fs: FakeFilesystem):
+    """Check that the verify_checksum function verifies integrity when size less than 64 KiB."""
+    verify_files_csv(fs=fs, magnitude="less")
+
+
+def test_verify_checksum_more_than_chunk_csv(fs: FakeFilesystem):
+    """Check that the verify_checksum function verifies integrity when size more than 64 KiB."""
+    verify_files_csv(fs=fs, magnitude="more")
+

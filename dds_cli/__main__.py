1404ce69626df0857b7272b7befef10d22224bce
valyo <582646+valyo@users.noreply.github.com>
2022-10-28 13:24:46 +0200

    better message
    Co-authored-by: Ina Odén Österbo <35953392+inaod568@users.noreply.github.com>

--- dds_cli/__main__.py
+++ dds_cli/__main__.py
@@ -1607,7 +1607,7 @@ def get_data(
         sys.exit(1)
     elif not get_all and not (source or source_path_file):
         LOG.error(
-            "The command 'dds data get' reuires either the flag '--get-all' or some of the options '--source'/'--source-path-fail'."
+            "Specify either '--source'/'--source-path-file' to download specific directories / files, or '--get-all' to download all project contents."
         )
         sys.exit(1)
 

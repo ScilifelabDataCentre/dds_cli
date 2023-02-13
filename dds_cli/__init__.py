d4167bdd75ab6b39f0546f520acccbac3b99df3e
Ina Odén Österbo <i-oden@users.noreply.github.com>
2023-02-13 15:55:10 +0100

    black

--- dds_cli/__init__.py
+++ dds_cli/__init__.py
@@ -60,7 +60,7 @@ class DDSEndpoint:
     BASE_ENDPOINT_DOCKER = "http://dds_backend:5000/api/v1"
     BASE_ENDPOINT_REMOTE = "https://delivery.scilifelab.se/api/v1"
     BASE_ENDPOINT_REMOTE_TEST = "https://dds-dev.dckube.scilifelab.se/api/v1"
-    BASE_ENDPOINT_REMOTE_SAFESPRING = "" # TODO: Add new url here
+    BASE_ENDPOINT_REMOTE_SAFESPRING = ""  # TODO: Add new url here
     if os.getenv("DDS_CLI_ENV") == "development":
         BASE_ENDPOINT = BASE_ENDPOINT_LOCAL
     elif os.getenv("DDS_CLI_ENV") == "docker-dev":

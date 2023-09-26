c096f748d1bfbfb901e77fef4dab972ab20032f8
rv0lt <a.revueltam@alumnos.upm.es>
2023-09-26 13:42:15 +0200

    added functionality

--- dds_cli/project_status.py
+++ dds_cli/project_status.py
@@ -2,10 +2,12 @@
 import datetime
 import logging
 import typing
+import sys
 
 # Installed
 import pytz
 import tzlocal
+import rich
 
 # Own modules
 from dds_cli import base
@@ -44,6 +46,24 @@ class ProjectStatusManager(base.DDSBaseClass):
         self.project = project
 
     # Public methods ###################### Public methods #
+
+    def get_project_info(self):
+        """Collect project information from API."""
+        # Get info about a project from API
+        response, _ = dds_cli.utils.perform_request(
+            DDSEndpoint.PROJ_INFO,
+            method="get",
+            headers=self.token,
+            params={"project": self.project},
+            error_message="Failed to get project information",
+        )
+
+        project_info = response.get("project_info")
+        if not project_info:
+            raise dds_cli.exceptions.ApiResponseError(message="No project information to display.")
+
+        return project_info
+    
     def get_status(self, show_history):
         """Get current status and status history of the project."""
         resp_json, _ = dds_cli.utils.perform_request(
@@ -101,6 +121,28 @@ class ProjectStatusManager(base.DDSBaseClass):
             extra_params["deadline"] = deadline
         if is_aborted:
             extra_params["is_aborted"] = is_aborted
+        
+        # If the status is going to be archived or deleted. Ask for confirmation
+        if new_status in ["Archived","Deleted"]:
+
+            #get project info
+            project_info = self.get_project_info()
+
+            # Create confirmation prompt with project info
+            question = f"Are you sure you want to modify the status of {self.project}? All its contents "
+            if new_status == 'Deleted':
+                question = question + "and metainfo "
+            question += (
+                "will be deleted!\n"
+                f"The project '[b]{self.project}[/b]' is about to be [b][blue]{new_status}[/blue][/b].\n"
+                f"Title: \t{project_info['Title']}\n"
+                f"Description: \t{project_info['Description']}\n"
+                f"PI: \t{project_info['PI']}\n"
+            )
+
+            if not rich.prompt.Confirm.ask(question):
+                LOG.info("Probably for the best. Exiting.")
+                sys.exit(0)
 
         response_json, _ = dds_cli.utils.perform_request(
             endpoint=DDSEndpoint.UPDATE_PROJ_STATUS,

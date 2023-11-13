4d774e59d58bf243119d783dd9d63a77f2ed7c2d
Ina Odén Österbo <i-oden@users.noreply.github.com>
2023-11-13 15:17:38 +0100

    version

--- dds_cli/version.py
+++ dds_cli/version.py
@@ -1,3 +1,4 @@
 """DDS CLI version."""
-
-__version__ = "2.5.2"
+# Do not change bump the major version unless absolutely necessary - makes incompatible with API
+# If mid or minor version reaches 9, continue to 10, 11 etc.
+__version__ = "2.6.0"

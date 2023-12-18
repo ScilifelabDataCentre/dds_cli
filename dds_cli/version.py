57b2c1417301be51c60dbd5594abbcfdf15afafb
Valentin Georgiev <valentin.georgiev@scilifelab.uu.se>
2023-12-18 11:09:00 +0100

    Revert "Revert "New release 2023-12-20""

--- dds_cli/version.py
+++ dds_cli/version.py
@@ -1,4 +1,4 @@
 """DDS CLI version."""
 # Do not change bump the major version unless absolutely necessary - makes incompatible with API
 # If mid or minor version reaches 9, continue to 10, 11 etc.
-__version__ = "2.6.0"
+__version__ = "2.6.1"

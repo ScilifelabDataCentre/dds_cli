ff5d59b31c0f4de4020b2e48a607d5b1fea378b2
rv0lt <alvarorevu@gmail.com>
2024-09-20 09:10:32 +0200

    undo version change

--- dds_cli/version.py
+++ dds_cli/version.py
@@ -2,4 +2,4 @@
 
 # Do not change bump the major version unless absolutely necessary - makes incompatible with API
 # If mid or minor version reaches 9, continue to 10, 11 etc.
-__version__ = "2.7.1.dev0"
+__version__ = "2.7.1"

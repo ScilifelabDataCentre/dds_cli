668a5253c4f4184a9130b686a663fd8f53f4de61
rv0lt <alvarorevu@gmail.com>
2024-10-03 11:12:40 +0200

    new option to send motd to only unit personnel

--- dds_cli/__main__.py
+++ dds_cli/__main__.py
@@ -2070,15 +2070,16 @@ def deactivate_motd(click_ctx, motd_id):
 # -- dds motd send -- #
 @motd_group_command.command(name="send")
 @click.argument("motd_id", metavar="[MOTD_ID]", nargs=1, type=int, required=True)
+@click.option("--unit-personnel-only", is_flag=True, required=False, default=False, help="Send MOTD to unit personnel only.")
 @click.pass_obj
-def send_motd(click_ctx, motd_id):
+def send_motd(click_ctx, motd_id, unit_personnel_only):
     """Send motd as email to all users."""
     try:
         with dds_cli.motd_manager.MotdManager(
             no_prompt=click_ctx.get("NO_PROMPT", False),
             token_path=click_ctx.get("TOKEN_PATH"),
         ) as sender:
-            sender.send_motd(motd_id=motd_id)
+            sender.send_motd(motd_id=motd_id, unit_personnel_only=unit_personnel_only)
     except (
         dds_cli.exceptions.AuthenticationError,
         dds_cli.exceptions.ApiResponseError,

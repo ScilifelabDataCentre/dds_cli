e552fc0b37281faf61d9d0dafc7cf87632d4be5a
ebba-eliasson <ebba.eliasson@scilifelab.se>
2025-03-26 13:51:52 +0100

    Add req to exe build

--- gui_build/gui_build.sh
+++ gui_build/gui_build.sh
@@ -1,2 +1,4 @@
 pip install pyinstaller
-pyinstaller --onefile --name dds_gui_standalone --hidden-import textual.widgets._tab_pane gui_standalone.py
+pip install -r ../requirements.txt
+pip install -r ../requirements-dev.txt
+pyinstaller --onefile --name dds_gui_standalone --hidden-import textual.widgets._tab_pane --hidden-import cgi gui_standalone.py

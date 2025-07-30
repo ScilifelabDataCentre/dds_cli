4a0a56c754fb513a7b70436f47f53f5d87970a7f
j-awada <jana.awada@scilifelab.uu.se>
2025-07-30 13:50:19 +0200

    run black

--- dds_cli/data_getter.py
+++ dds_cli/data_getter.py
@@ -149,7 +149,7 @@ class DataGetter(base.DDSBaseClass):
                 LOG.debug(
                     "Downloaded file size mismatch: expected %s bytes, got %s bytes",
                     expected_size,
-                    actual_size
+                    actual_size,
                 )
 
         if file_size_check:
@@ -182,12 +182,14 @@ class DataGetter(base.DDSBaseClass):
                 expected_size = file_info["size_original"]
                 actual_size = pathlib.Path(file).stat().st_size
                 if actual_size == expected_size:
-                    LOG.debug("Downloaded file size matches expected size: %s bytes.", expected_size)
+                    LOG.debug(
+                        "Downloaded file size matches expected size: %s bytes.", expected_size
+                    )
                 else:
                     LOG.debug(
                         "Downloaded file size mismatch: expected %s bytes, got %s bytes",
                         expected_size,
-                        actual_size
+                        actual_size,
                     )
                 # TODO (ina): decide on checksum verification method --
                 # this checks original, the other is generated from compressed

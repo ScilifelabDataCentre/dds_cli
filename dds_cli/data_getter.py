1c44fb2b17335a3b53a6c6ec4961d8a8ff89257b
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-08-21 11:44:07 +0200

    black

--- dds_cli/data_getter.py
+++ dds_cli/data_getter.py
@@ -146,10 +146,14 @@ class DataGetter(base.DDSBaseClass):
 
             if actual_size == expected_size:
                 file_size_verified = True
-                LOG.debug("Downloaded file '%s' size matches expected size: %s bytes.", file_name_in_db, expected_size)
+                LOG.debug(
+                    "Downloaded file '%s' size matches expected size: %s bytes.",
+                    file_name_in_db,
+                    expected_size,
+                )
             else:
                 LOG.debug(
-                    "Downloaded file '%s' size mismatch: expected %s bytes, got %s bytes. Not decrypting.", 
+                    "Downloaded file '%s' size mismatch: expected %s bytes, got %s bytes. Not decrypting.",
                     file_name_in_db,
                     expected_size,
                     actual_size,
@@ -194,9 +198,9 @@ class DataGetter(base.DDSBaseClass):
                 actual_size = pathlib.Path(file).stat().st_size
                 if actual_size == expected_size:
                     LOG.debug(
-                        "Downloaded file '%s' size matches expected size: %s bytes.", 
-                        file_name_in_db, 
-                        expected_size
+                        "Downloaded file '%s' size matches expected size: %s bytes.",
+                        file_name_in_db,
+                        expected_size,
                     )
                 else:
                     LOG.debug(

824aa108b050a3c332bfdc41b2ca76080fc0d1c1
Ina Odén Österbo <35953392+i-oden@users.noreply.github.com>
2025-08-22 14:00:27 +0200

    Use Path.home for DDSApp token path

--- gui_build/gui_standalone.py
+++ gui_build/gui_standalone.py
@@ -2,8 +2,10 @@
 This file is used to run the DDS GUI standalone for the executable.
 """
 
+from pathlib import Path
+
 from dds_cli.dds_gui.app import DDSApp
 
 if __name__ == "__main__":
-    app = DDSApp(token_path="~/.dds_cli_token")
+    app = DDSApp(token_path=str(Path.home() / ".dds_cli_token"))
     app.run()

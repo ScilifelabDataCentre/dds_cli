993b158ae138a0f2ea3fb8cc346be074b0f172b4
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-08-22 14:14:44 +0200

    pathlib

--- gui_build/gui_standalone.py
+++ gui_build/gui_standalone.py
@@ -2,10 +2,10 @@
 This file is used to run the DDS GUI standalone for the executable.
 """
 
-from pathlib import Path
+import pathlib
 
 from dds_cli.dds_gui.app import DDSApp
 
 if __name__ == "__main__":
-    app = DDSApp(token_path=str(Path.home() / ".dds_cli_token"))
+    app = DDSApp(token_path=str(pathlib.Path.home() / ".dds_cli_token"))
     app.run()

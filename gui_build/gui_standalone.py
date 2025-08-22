498fc2de4b4c093163a0acd2d52adc2e356394b7
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-08-22 13:37:58 +0200

    pathlib not Path imported

--- gui_build/gui_standalone.py
+++ gui_build/gui_standalone.py
@@ -1,6 +1,6 @@
 """Run the DDS GUI standalone executable."""
 
-from pathlib import Path
+import pathlib
 
 import pathlib
 
@@ -8,6 +8,6 @@ from dds_cli.dds_gui.app import DDSApp
 
 
 if __name__ == "__main__":
-    token_path = Path("custom") / "token" / "path"
+    token_path = pathlib.Path("custom") / "token" / "path"
     app = DDSApp(token_path=str(token_path))
     app.run()

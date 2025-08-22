ec5bb3ee57681b71d41eec524cf47bb3513028ec
Ina Odén Österbo <35953392+i-oden@users.noreply.github.com>
2025-08-22 14:19:28 +0200

    Update gui_build/gui_standalone.py

--- gui_build/gui_standalone.py
+++ gui_build/gui_standalone.py
@@ -4,7 +4,6 @@ This file is used to run the DDS GUI standalone for the executable.
 
 from dds_cli.dds_gui.app import DDSApp
 
-
 if __name__ == "__main__":
     token_path = pathlib.Path("custom") / "token" / "path"
     app = DDSApp(token_path=str(token_path))

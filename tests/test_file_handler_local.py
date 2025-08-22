f7edd9fc6d7d4a506bc7f57a249e8be6b96b6402
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-08-22 13:23:06 +0200

    don't import path

--- tests/test_file_handler_local.py
+++ tests/test_file_handler_local.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import pathlib
 from pyfakefs.fake_filesystem import FakeFilesystem
 from dds_cli.file_handler_local import LocalFileHandler
 
@@ -6,59 +6,59 @@ from dds_cli.file_handler_local import LocalFileHandler
 def test_localfilehandler_with_destination(fs: FakeFilesystem):
     """Test that the destination flag works."""
     # Create directories
-    fs.create_dir(Path("parentdir"))
-    fs.create_dir(Path("parentdir") / "somedir")
-    fs.create_dir(Path("parentdir") / "somedir" / "subdir")
+    fs.create_dir(pathlib.Path("parentdir"))
+    fs.create_dir(pathlib.Path("parentdir") / "somedir")
+    fs.create_dir(pathlib.Path("parentdir") / "somedir" / "subdir")
 
     # Verify exists
-    assert fs.exists(file_path=Path("parentdir"))
-    assert fs.exists(file_path=Path("parentdir") / "somedir")
-    assert fs.exists(file_path=Path("parentdir") / "somedir" / "subdir")
+    assert fs.exists(file_path=pathlib.Path("parentdir"))
+    assert fs.exists(file_path=pathlib.Path("parentdir") / "somedir")
+    assert fs.exists(file_path=pathlib.Path("parentdir") / "somedir" / "subdir")
 
     # Create files
-    fs.create_file(Path("parentdir") / "fileinparentdir.file")
-    fs.create_file(Path("parentdir") / "somedir" / "fileinsomedir.file")
-    fs.create_file(Path("parentdir") / "somedir" / "subdir" / "fileinsubdir.file")
+    fs.create_file(pathlib.Path("parentdir") / "fileinparentdir.file")
+    fs.create_file(pathlib.Path("parentdir") / "somedir" / "fileinsomedir.file")
+    fs.create_file(pathlib.Path("parentdir") / "somedir" / "subdir" / "fileinsubdir.file")
 
     # Verify files exist
-    assert fs.exists(file_path=Path("parentdir") / "fileinparentdir.file")
-    assert fs.exists(file_path=Path("parentdir") / "somedir" / "fileinsomedir.file")
-    assert fs.exists(file_path=Path("parentdir") / "somedir" / "subdir" / "fileinsubdir.file")
+    assert fs.exists(file_path=pathlib.Path("parentdir") / "fileinparentdir.file")
+    assert fs.exists(file_path=pathlib.Path("parentdir") / "somedir" / "fileinsomedir.file")
+    assert fs.exists(file_path=pathlib.Path("parentdir") / "somedir" / "subdir" / "fileinsubdir.file")
 
     # Verify that fails with incorrect paths
-    assert not fs.exists(file_path=Path("anotherdir"))
-    assert not fs.exists(file_path=Path("parentdir") / "somefile.file")
+    assert not fs.exists(file_path=pathlib.Path("anotherdir"))
+    assert not fs.exists(file_path=pathlib.Path("parentdir") / "somefile.file")
 
     # Call LocalFileHandler
     filehandler = LocalFileHandler(
-        user_input=((Path("parentdir"),), None),
+        user_input=((pathlib.Path("parentdir"),), None),
         project="someproject",
         temporary_destination="temporarydestination",
         remote_destination="remote_destination",
     )
     expected_data_1 = {
-        (Path("remote_destination") / "parentdir" / "fileinparentdir.file").as_posix(): {
-            "path_raw": Path.cwd() / "parentdir" / "fileinparentdir.file",
-            "subpath": Path("remote_destination") / "parentdir",
+        (pathlib.Path("remote_destination") / "parentdir" / "fileinparentdir.file").as_posix(): {
+            "path_raw": pathlib.Path.cwd() / "parentdir" / "fileinparentdir.file",
+            "subpath": pathlib.Path("remote_destination") / "parentdir",
             "size_raw": 0,
             "compressed": False,
             "path_processed": filehandler.create_encrypted_name(
-                raw_file=Path("parentdir") / "fileinparentdir.file",
-                subpath=Path("remote_destination") / "parentdir",
+                raw_file=pathlib.Path("parentdir") / "fileinparentdir.file",
+                subpath=pathlib.Path("remote_destination") / "parentdir",
                 no_compression=False,
             ),
             "size_processed": 0,
             "overwrite": False,
             "checksum": "",
         },
-        (Path("remote_destination") / "parentdir" / "somedir" / "fileinsomedir.file").as_posix(): {
-            "path_raw": Path.cwd() / "parentdir" / "somedir" / "fileinsomedir.file",
-            "subpath": Path("remote_destination") / "parentdir" / "somedir",
+        (pathlib.Path("remote_destination") / "parentdir" / "somedir" / "fileinsomedir.file").as_posix(): {
+            "path_raw": pathlib.Path.cwd() / "parentdir" / "somedir" / "fileinsomedir.file",
+            "subpath": pathlib.Path("remote_destination") / "parentdir" / "somedir",
             "size_raw": 0,
             "compressed": False,
             "path_processed": filehandler.create_encrypted_name(
-                raw_file=Path("parentdir") / "somedir" / "fileinsomedir.file",
-                subpath=Path("remote_destination") / "parentdir" / "somedir",
+                raw_file=pathlib.Path("parentdir") / "somedir" / "fileinsomedir.file",
+                subpath=pathlib.Path("remote_destination") / "parentdir" / "somedir",
                 no_compression=False,
             ),
             "size_processed": 0,
@@ -66,15 +66,15 @@ def test_localfilehandler_with_destination(fs: FakeFilesystem):
             "checksum": "",
         },
         (
-            Path("remote_destination") / "parentdir" / "somedir" / "subdir" / "fileinsubdir.file"
+            pathlib.Path("remote_destination") / "parentdir" / "somedir" / "subdir" / "fileinsubdir.file"
         ).as_posix(): {
-            "path_raw": Path.cwd() / "parentdir" / "somedir" / "subdir" / "fileinsubdir.file",
-            "subpath": Path("remote_destination") / "parentdir" / "somedir" / "subdir",
+            "path_raw": pathlib.Path.cwd() / "parentdir" / "somedir" / "subdir" / "fileinsubdir.file",
+            "subpath": pathlib.Path("remote_destination") / "parentdir" / "somedir" / "subdir",
             "size_raw": 0,
             "compressed": False,
             "path_processed": filehandler.create_encrypted_name(
-                raw_file=Path("parentdir") / "somedir" / "subdir" / "fileinsubdir.file",
-                subpath=Path("remote_destination") / "parentdir" / "somedir" / "subdir",
+                raw_file=pathlib.Path("parentdir") / "somedir" / "subdir" / "fileinsubdir.file",
+                subpath=pathlib.Path("remote_destination") / "parentdir" / "somedir" / "subdir",
                 no_compression=False,
             ),
             "size_processed": 0,

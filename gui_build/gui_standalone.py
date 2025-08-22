814408ea718da054528e158e1f68681cc880ae86
Ina Odén Österbo <35953392+i-oden@users.noreply.github.com>
2025-08-22 14:20:06 +0200

    Update gui_build/gui_standalone.py

--- gui_build/gui_standalone.py
+++ gui_build/gui_standalone.py
@@ -7,6 +7,5 @@ import pathlib
 from dds_cli.dds_gui.app import DDSApp
 
 if __name__ == "__main__":
-    token_path = pathlib.Path("custom") / "token" / "path"
-    app = DDSApp(token_path=str(token_path))
+    app = DDSApp(token_path="~/.dds_cli_token")
     app.run()

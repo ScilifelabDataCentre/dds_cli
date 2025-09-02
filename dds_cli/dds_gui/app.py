9e8e68d3a1fb11cf7f54fb24020c03aa10f4b31d
ebba-eliasson <ebba.eliasson@scilifelab.se>
2025-09-02 10:47:51 +0200

    change comment

--- dds_cli/dds_gui/app.py
+++ dds_cli/dds_gui/app.py
@@ -33,7 +33,7 @@ theme = Theme(
 )
 
 
-class DDSApp(DDSStateManager):  ### Moved app to state manager?
+class DDSApp(DDSStateManager):  ### Moved Textual App class to State Manager to access notifications
     """Textual App for DDS CLI."""
 
     def __init__(self, token_path: str):

f33fd38e5b8dc59b78ed393400c670c7a75ec8d1
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-09-11 13:18:29 +0200

    black

--- tests/test_data_getter.py
+++ tests/test_data_getter.py
@@ -10,9 +10,10 @@ from dds_cli import constants
 
 # Tests
 
+
 def test_get_uses_timeout(monkeypatch, tmp_path):
     """Test that DataGetter.get uses the correct timeout values.
-    
+
     monkeypatch is a pytest fixture that allows you to modify objects temporatily.
     tmp_path is a pytest fixture that provides a temporary directory.
     """
@@ -21,8 +22,8 @@ def test_get_uses_timeout(monkeypatch, tmp_path):
 
     # Mock filehandler with necessary data
     # Using SimpleNamespace because it allows you to create simple objects
-    # with attributes without defining a custom class 
-    # Here we use it to mock the filehandler instead of initializing 
+    # with attributes without defining a custom class
+    # Here we use it to mock the filehandler instead of initializing
     # the full FileHandler class which requires more inputs etc
     # Could technically also use Filehandler.__new__(FileHandler) but this is cleaner
     getter.filehandler = SimpleNamespace(
@@ -36,13 +37,13 @@ def test_get_uses_timeout(monkeypatch, tmp_path):
     )
 
     # Create mock objects
-    progress = MagicMock() # needed for get method but doesn't invoke real progress
+    progress = MagicMock()  # needed for get method but doesn't invoke real progress
     mock_response = MagicMock()
 
     # Define the mock return values
     mock_response.__enter__.return_value = mock_response
     mock_response.__exit__.return_value = False
-    mock_response.iter_content.return_value = [b"data"] # Simulate content chunks
+    mock_response.iter_content.return_value = [b"data"]  # Simulate content chunks
     mock_response.raise_for_status.return_value = None  # Used in download to check for HTTP errors
 
     # Mock the requests.get method to return the mock_response
